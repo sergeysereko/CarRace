da88edfde2aae1df94b5ed5c9c236c5a3fd66904
sergey <sergeysereko@gmail.com>
2024-01-11 13:57:59 +0200

    Добавил класс RaceCarRunnable

--- CarRace/src/Car/RaceCarRunnable.java
+++ CarRace/src/Car/RaceCarRunnable.java
@@ -1,4 +1,53 @@
 package Car;
+import java.util.Random;
 
-public class RaceCarRunnable {
+public class RaceCarRunnable extends Car {
+
+    private int passed;
+    private int distance;
+    private boolean isFinish;
+
+    public RaceCarRunnable(String name, int maxSpeed, int passed, int distance, boolean isFinish) {
+        super(name, maxSpeed);
+        this.passed = passed;
+        this.distance = distance;
+        this.isFinish = isFinish;
+
+    }
+    public RaceCarRunnable(String name, int maxSpeed, int distance) {
+        super(name, maxSpeed);
+        this.distance = distance;
+    }
+
+    private int getRandomSpeed() {
+        Random random = new Random();
+        return random.nextInt((getMaxSpeed() / 2) + 1) + getMaxSpeed() / 2;
+    }
+
+    // Переопределение метода run() для выполнения в потоке
+    @Override
+    public void run() {
+        while (!isFinish) {
+            int speed = getRandomSpeed();
+            passed += speed;
+            if (passed >= distance) {
+                passed = distance;
+                isFinish = true;
+            }
+
+            System.out.println(getCarInfo(speed, passed, distance));
+
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    // информация о состоянии машины
+    private String getCarInfo(int speed, int progress, int totalDistance) {
+        return getName() + " => speed: " + speed + "; progress: " + progress + "/" + totalDistance;
+    }
+
 }
